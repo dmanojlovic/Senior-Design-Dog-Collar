a3675df8de601d4fb7a135ec7956f73079dd8906
Nick Durnil <nickdurnil@gmail.com>
2024-09-11 11:36:14 -0400

    Added geofence data

--- Server/Website 2.0/app.js
+++ Server/Website 2.0/app.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+var fs = require('fs');
 
 require('dotenv').config();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+app.post("/geofence", (req, res) => {
+	fs.writeFile('public/geofence.json', JSON.stringify(req.body), 'utf8', err => { 
+        if (err) throw err;
+    });
+	res.send("recieved");
+});
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'));
--- Server/Website 2.0/public/jsonLoader.js
+++ Server/Website 2.0/public/jsonLoader.js
@@ -1,15 +1,37 @@
-function initMap(latlng){
+async function initMap(latlng){
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 18,
         center: latlng,
         mapTypeId: "terrain"
     });
 
-    const posArea = new google.maps.Circle({
+    const res = await fetch('geofence.json');
+
+    const fenceData = await res.json();
+
+    const fenceCoords = {
+        north: fenceData.north,
+        south: fenceData.south,
+        east:  fenceData.east,
+        west:  fenceData.west
+    }
+
+    const geofence = new google.maps.Rectangle({
+        bounds: fenceCoords,
         strokeColor: "#FF0000",
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        editable: true,
+        draggable: true
+      });
+
+    const posArea = new google.maps.Circle({
+        strokeColor: "#00FF00",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#00FF00",
         fillOpacity: 0.35,
         map,
         center: latlng,
@@ -17,12 +39,29 @@ function initMap(latlng){
     });
 
     posArea.setMap(map);
+    geofence.setMap(map);
+    ["bounds_changed", "dragstart", "drag", "dragend"].forEach((eventName) => {
+        geofence.addListener(eventName, () => {
+          fetch("/geofence", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'       
+        },
+        body: JSON.stringify(geofence.getBounds())
+    }).catch(err => {
+        console.log(err);
+    })
+        });
+    });
 }
 
 function jsonLoad() {
     fetch('data.json').then(response => response.json()).then(data => {
     const battery = document.getElementById("battery");
     battery.textContent = "Battery Life: " + data.life + "%";
+    const time = document.getElementById("time");
+    time.textContent = "Last Recorded: " + data.time;
+
 
     initMap(data.location);
 
